033803efcbdb67d76dfbd31c284f53626a01b714
Ian Restrepo <91353384+IanRestrepo@users.noreply.github.com>
2023-11-21 19:27:19 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -16,18 +16,20 @@ document.querySelector(".Switch-Theme").addEventListener('click', ()=> {
 })
 
 document.querySelector(".searchBtn").addEventListener("change", function (e) {
- 
+ e.preventDefault()
     var inputValue = document.querySelector(".input").value;
     var userInput = getUserInput();
     searchGiphy(userInput);
 
-       e.preventDefault()
+    e.preventDefault()   
 });
 
 document.querySelector(".searchBtn").addEventListener("click", function () {
+    e.preventDefault()
     var inputValue = document.querySelector(".input").value;
     var userInput = getUserInput();
     searchGiphy(userInput);
+    e.preventDefault()
 });
 
 
